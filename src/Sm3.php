f0f67e648356c3f494ca91c20f877594a4ae0d71
lat751608899 <751608899@qq.com>
2022-05-19 17:40:51 +0800

    解决php5.6 Int 溢出问题
    解决php5.6 Int 溢出导致计算错误解密失败问题

--- src/Sm3.php
+++ src/Sm3.php
@@ -32,26 +32,28 @@ class Sm3
         }
         $bytes = array_merge($bytes, $packBytes);
         $w = $w1 = [];
-        $a = 0x7380166f;
-        $b = 0x4914b2b9;
-        $c = 0x172442d7;
-        $d = 0xda8a0600;
-        $e = 0xa96f30bc;
-        $f = 0x163138aa;
-        $g = 0xe38dee4d;
-        $h = 0xb0fb0e4e;
+        $a = gmp_init('0x7380166f', 16);
+        $b = gmp_init('0x4914b2b9', 16);
+        $c = gmp_init('0x172442d7', 16);
+        $d = gmp_init('0xda8a0600', 16);
+        $e = gmp_init('0xa96f30bc', 16);
+        $f = gmp_init('0x163138aa', 16);
+        $g = gmp_init('0xe38dee4d', 16);
+        $h = gmp_init('0xb0fb0e4e', 16);
         while (count($bytes) >= 64) {
             for ($i = 0; $i < 16; $i++) {
-                $tmpStr = $this->bytesToStr(array_slice($bytes, 4 * $i, 4));
-                $w[$i] = @unpack('N', $tmpStr)[1];
+                $w[$i] = $this->bytesToInt(array_slice($bytes, 4 * $i, 4));
             }
             for ($i = 16; $i < 68; $i++) {
                 $w13 = $this->leftRotate($w[$i - 13], 7);
-                $p1 = $w[$i - 16] ^ $w[$i - 9] ^ $this->leftRotate($w[$i - 3], 15);
-                $w[$i] = $this->p1($p1) ^ $w13 ^ $w[$i - 6];
+                $p1 = gmp_xor(gmp_xor($w[$i - 16], $w[$i - 9]), $this->leftRotate($w[$i - 3], 15));
+                //$p1 = $w[$i - 16] ^ $w[$i - 9] ^ $this->leftRotate($w[$i - 3], 15);
+                //$w[$i] = $this->p1($p1) ^ $w13 ^ $w[$i - 6];
+                $w[$i] = gmp_xor(gmp_xor($this->p1($p1), $w13), $w[$i - 6]);
             }
             for ($i = 0; $i < 64; $i++) {
-                $w1[$i] = $w[$i] ^ $w[$i + 4];
+//                $w1[$i] = $w[$i] ^ $w[$i + 4];
+                $w1[$i] = gmp_xor($w[$i], $w[$i + 4]);
             }
             $A = $a;
             $B = $b;
@@ -62,10 +64,11 @@ class Sm3
             $G = $g;
             $H = $h;
             for ($i = 0; $i < 64; $i++) {
-                $a1 = $this->leftRotate($i < 16 ? 0x79cc4519 : 0x7a879d8a, $i);
+                $a1 = $this->leftRotate(gmp_init($i < 16 ? '0x79cc4519' : '0x7a879d8a', 16), $i);
                 $a12 = $this->leftRotate($A, 12);
                 $SS1 = $this->leftRotate($this->add($a12, $E, $a1), 7);
-                $SS2 = $SS1 ^ $this->leftRotate($A, 12);
+//                $SS2 = $SS1 ^ $this->leftRotate($A, 12);
+                $SS2 = gmp_xor($SS1, $this->leftRotate($A, 12));
                 if ($i < 16) {
                     $TT1 = $this->add($this->ff0($A, $B, $C), $D, $SS2, $w1[$i]);
                     $TT2 = $this->add($this->gg0($E, $F, $G), $H, $SS1, $w[$i]);
@@ -82,14 +85,22 @@ class Sm3
                 $F = $E;
                 $E = $this->p0($TT2);
             }
-            $a ^= $A;
+            $a = gmp_xor($a, $A);
+            $b = gmp_xor($b, $B);
+            $c = gmp_xor($c, $C);
+            $d = gmp_xor($d, $D);
+            $e = gmp_xor($e, $E);
+            $f = gmp_xor($f, $F);
+            $g = gmp_xor($g, $G);
+            $h = gmp_xor($h, $H);
+            /*$a ^= $A;
             $b ^= $B;
             $c ^= $C;
             $d ^= $D;
             $e ^= $E;
             $f ^= $F;
             $g ^= $G;
-            $h ^= $H;
+            $h ^= $H;*/
             $bytes = array_splice($bytes, 64);
         }
 
@@ -100,31 +111,37 @@ class Sm3
 
     protected function ff0($x, $y, $z)
     {
+        return gmp_xor(gmp_xor($x, $y), $z);
         return $x ^ $y ^ $z;
     }
 
     protected function gg0($x, $y, $z)
     {
+        return gmp_xor(gmp_xor($x, $y), $z);
         return $x ^ $y ^ $z;
     }
 
     protected function ff1($x, $y, $z)
     {
+        return gmp_or(gmp_or(gmp_and($x, $y), gmp_and($x, $z)), gmp_and($y, $z));
         return ($x & $y) | ($x & $z) | ($y & $z);
     }
 
     protected function gg1($x, $y, $z)
     {
+        return gmp_or(gmp_and($x, $y), gmp_and(gmp_neg(gmp_add($x, 1)), $z));
         return ($x & $y) | (~$x & $z);
     }
 
     protected function p0($x)
     {
+        return gmp_xor(gmp_xor($x, $this->leftRotate($x, 9)), $this->leftRotate($x, 17));
         return $x ^ $this->leftRotate($x, 9) ^ $this->leftRotate($x, 17);
     }
 
     protected function p1($x)
     {
+        return gmp_xor(gmp_xor($x, $this->leftRotate($x, 15)), $this->leftRotate($x, 23));
         return $x ^ $this->leftRotate($x, 15) ^ $this->leftRotate($x, 23);
     }
 }
--- src/Util.php
+++ src/Util.php
@@ -11,7 +11,7 @@ trait Util
             $hex = "0" . $hex;
         }
         $bytes = [];
-        $len = strlen($hex);
+        $len   = strlen($hex);
         for ($i = 0; $i < $len; $i++) {
             $bytes[] = (int)base_convert($hex[$i] . $hex[++$i], 16, 10);
         }
@@ -23,7 +23,8 @@ trait Util
     {
         $hex = '';
         foreach ($data as $value) {
-            $hex .= $this->getHex(base_convert($value, 10, 16));
+//            $hex .= $this->getHex(base_convert($value, 10, 16));
+            $hex .= $this->getHex(gmp_strval($value, 16));
         }
 
         return $hex;
@@ -32,6 +33,11 @@ trait Util
     public function leftRotate($x, $i)
     {
         $i %= 32;
+        $aa = pow(2, $i);
+        $a = gmp_init(strval($aa), 10);
+        $b = gmp_init('0xFFFFFFFF', 16);
+        $c = gmp_init(strval(pow(2, 32 - $i)), 10);
+        return gmp_or(gmp_and(gmp_mul($x, $a), $b), gmp_div($x, $c));
 
         return (($x << $i) & 0xFFFFFFFF) | ($x >> (32 - $i));
     }
@@ -43,15 +49,42 @@ trait Util
 
     public function bytesToStr($bytes)
     {
-        array_unshift($bytes,'C'.count($bytes));
+        array_unshift($bytes, 'C' . count($bytes));
 
         return call_user_func_array('pack', $bytes);
     }
 
+    public function bytesToInt($bytes)
+    {
+        $val = 0;
+        $pos = 0;
+
+        $val = $bytes[$pos + 0] & 0xff;
+
+        $val <<= 8;
+
+        $val |= $bytes[$pos + 1] & 0xff;
+
+        $val <<= 8;
+
+        $val |= $bytes[$pos + 2] & 0xff;
+        $val = gmp_or(gmp_mul(gmp_init($val), gmp_init(pow(2, 8))), gmp_init($bytes[$pos + 3] & 0xff));
+
+        return $val;
+    }
+
     public function add(...$a)
     {
-        $sum = array_sum($a);
-        $sum > 0xFFFFFFFF && $sum &= 0xFFFFFFFF;
+        $sum = 0;
+        foreach ($a as $value) {
+            $sum = gmp_add($sum, $value);
+        }
+        $base = gmp_init('0xFFFFFFFF', 16);
+        if (gmp_cmp($sum, $base)) {
+            $sum = gmp_and($sum, $base);
+        }
+        /*$sum = array_sum($a);
+        $sum > 0xFFFFFFFF && $sum &= 0xFFFFFFFF;*/
 
         return $sum;
     }
@@ -63,17 +96,18 @@ trait Util
 
     public function generate($numBits = 256)
     {
-        $value = gmp_random_bits($numBits);
-        $mask = gmp_sub(gmp_pow(2, $numBits), 1);
+        $value   = gmp_random_bits($numBits);
+        $mask    = gmp_sub(gmp_pow(2, $numBits), 1);
         $integer = gmp_and($value, $mask);
 
         return $integer;
     }
 
-    public function decHex(string $dec, $len = 0): string
+    public function decHex($dec, $len = 0)
     {
-        $dec = gmp_init($dec, 10);
-
+        if (!$dec instanceof \GMP) {
+            $dec = gmp_init($dec, 10);
+        }
         if (gmp_cmp($dec, 0) < 0) {
             throw new \Exception('Unable to convert negative integer to string');
         }
@@ -81,7 +115,7 @@ trait Util
         $hex = gmp_strval($dec, 16);
 
         if (strlen($hex) % 2 != 0) {
-            $hex = '0'.$hex;
+            $hex = '0' . $hex;
         }
         if ($len && strlen($hex) < $len) {  // point x y 要补齐 64 位
             $hex = str_pad($hex, $len, "0", STR_PAD_LEFT);
